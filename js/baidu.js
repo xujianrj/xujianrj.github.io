0bd447bb4646bceabb3fcc111ca18f1cbea2969e
hengxing204 <keys203@gmail.com>
2019-12-27 11:07:16 +0800

    update

---
+++ js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?11b6bd8a4024730a473033e62c5b1900";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
